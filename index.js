0af5307739563fcd6a0372f03774b820faf3191c
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-12 20:46:36 +0600

    if and else use in javaScript

---
+++ index.js
@@ -0,0 +1,32 @@
+var laptopPrice = 2200
+if(laptopPrice < 2201){
+console.log ("Ami laptp ti croy korbo");
+}
+else{
+    console.log("Ami croy korbo na");
+};
+
+var mobilePrice = 2202
+if(mobilePrice > 2201){
+console.log ("Ami mobile ti croy korbo");
+}
+else{
+    console.log("Ami mobile it croy korbo na");
+};
+
+
+var bonPrice = 5
+if(bonPrice != 5){
+console.log ("Ami bon ti croy korbo");
+}
+else{
+    console.log("Ami bon it croy korbo na");
+};
+
+var batarbonPrice = 15
+if(batarbonPrice == 15){
+console.log ("Ami batarbon ti croy korbo");
+}
+else{
+    console.log("Ami batarbon it croy korbo na");
+};
